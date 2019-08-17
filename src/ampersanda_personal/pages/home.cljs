4b900b229d11dc0239d512cdb60d0833d29d0703
Mochamad Lucky Pradana <luckypradana123@yandex.ua>
2019-08-17 09:47:32 +0700

    refactor

--- src/ampersanda_personal/pages/home.cljs
+++ src/ampersanda_personal/pages/home.cljs
@@ -10,4 +10,14 @@
  [:main
   {:class "content editor"}
   [:h1 {:class "jumbo use-global-slant"} "Hello!"]
-  [:h1 "My name is " [:span {:style {:color "var(--blue)"}} "Mochamad Lucky Pradana"] ". I am Front End Developer and (ocassionally) Flutter Developer who live in " [:a {:href "https://goo.gl/maps/sJumGDqZ8KVz16nG7"} "Surabaya, Indonesia"] "."]])
+  [:h1 "My name is " [:span {:style {:color "var(--blue)"}} "Mochamad Lucky Pradana"] ". I am Front End Developer and (ocassionally) Flutter Developer who live in " [:a {:href "https://goo.gl/maps/sJumGDqZ8KVz16nG7"} "Surabaya, Indonesia"] "."]
+
+  [:div {:class "grid-menu normal-cursor"}
+   [:div {:class "grid-menu--item" :style {:background :blue}}
+    [:a {:href "#"} "Stories"]]
+   [:div {:class "grid-menu--item" :style {:background :red}}
+    "Sub Sites"]
+   [:div {:class "grid-menu--item grid-menu--item__small grid-menu--item__square" :style {:background :green}}
+    "About"]
+   [:div {:class "grid-menu--item grid-menu--item__small grid-menu--item__square" :style {:background :purple}}
+    "Contact"]]])
